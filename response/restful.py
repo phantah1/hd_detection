4be939f9a55b53d2d2fbd9ecbd3eac535b9b403e
dungxibo123 <dungxibo123@github.com>
2020-08-25 16:51:48 +0700

    debug with post request haven't tested yet

--- response/restful.py
+++ response/restful.py
@@ -9,11 +9,8 @@ app = Flask(__name__)
 api = Api(app)
 
 class HumansCount(Resource):
-	def get(self):
-		'''
-		Test GET
-		'''
-		return {"hello": "oke"}
+	def get(self,img_path):
+		return {"hello": "test: {}".format(img_path)}
 	def post(self,img_path):
 		"""
 		receive a post request and respones a dictionary with 2 key
@@ -26,13 +23,13 @@ class HumansCount(Resource):
 		"""
 		count, img = recognize(img_path)
 		new_path = img_path + img_path.split('.')[0].split('input\\')[-1]
-		cv2.imwrite('output\\{new_path}.jpg')
-		dic = {'count': count, 'path': 'output\\{new_path}.jpg'}
+		cv2.imwrite('output\\{}.jpg'.format(new_path))
+		dic = {'count': count, 'path': 'output\\{}.jpg'.format(new_path)}
 		#do tao khong tra ve anh duoc nen tao tra ve relative path cua cai anh, m dung no de lay anh roi process len nha :v
 		return dic
 
 
-api.add_resource(HumansCount, '/humancounting') # Human counting route
+api.add_resource(HumansCount, '/humancounting/<string:img_path>') # Human counting route
 
 
 if __name__=='__main__':
